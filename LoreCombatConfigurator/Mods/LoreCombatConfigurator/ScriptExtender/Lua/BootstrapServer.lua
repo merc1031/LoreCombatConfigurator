647d5be09fcba2d2a93ccca72a6f09d384aecfc4
Leonidas Loucas <merc1031@gmail.com>
2024-01-17 21:30:08 +0000

    Version 0.9.2.0

--- LoreCombatConfigurator/Mods/LoreCombatConfigurator/ScriptExtender/Lua/BootstrapServer.lua
+++ LoreCombatConfigurator/Mods/LoreCombatConfigurator/ScriptExtender/Lua/BootstrapServer.lua
@@ -3436,7 +3436,7 @@ function DummySessionContext()
 end
 
 local function OnSessionLoaded()
-    _Log(DummySessionContext(), 0, "0.9.1.0")
+    _Log(DummySessionContext(), 0, "0.9.2.0")
 
     if IsCriticalMissLoaded() then
         ProtectedPassives["Passive_CriticalMiss"] = true
