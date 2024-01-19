579fa6cce4421538f6e94bc709e938b35d69aa40
Leonidas Loucas <merc1031@gmail.com>
2024-01-19 08:23:03 +0000

    fix: crash on nil subclass progression

--- LoreCombatConfigurator/Mods/LoreCombatConfigurator/ScriptExtender/Lua/BootstrapServer.lua
+++ LoreCombatConfigurator/Mods/LoreCombatConfigurator/ScriptExtender/Lua/BootstrapServer.lua
@@ -998,25 +998,29 @@ function GenerateClassSpellLists(sessionContext, blacklistedAbilitiesByClass, bl
         end
     end
     for subclassName, subclass in pairs(class.Subclasses) do
-        for level, progression in pairs(subclass.Progression) do
-            if not blacklistedAbilitiesByClass[progression.Name] then
-                for _, selector in ipairs(progression.AddSpells) do
-                    for spellId, spell in pairs(selector.Spells) do
-                        if not blacklistedAbilities[spell.Name] then
-                            spells[spell.Name] = {
-                                Spell = spell,
-                                ClassLevel = level,
-                            }
+        if subclass.Progression == nil then
+            sessionContext.LogI(2, 18, string.format("Subclass %s of class %s has no progression", subclass.Name, class.Name))
+        else
+            for level, progression in pairs(subclass.Progression) do
+                if not blacklistedAbilitiesByClass[progression.Name] then
+                    for _, selector in ipairs(progression.AddSpells) do
+                        for spellId, spell in pairs(selector.Spells) do
+                            if not blacklistedAbilities[spell.Name] then
+                                spells[spell.Name] = {
+                                    Spell = spell,
+                                    ClassLevel = level,
+                                }
+                            end
                         end
                     end
-                end
-                for _, selector in ipairs(progression.SelectSpells) do
-                    for spellId, spell in pairs(selector.Spells) do
-                        if not blacklistedAbilities[spell.Name] then
-                            spells[spell.Name] = {
-                                Spell = spell,
-                                ClassLevel = level,
-                            }
+                    for _, selector in ipairs(progression.SelectSpells) do
+                        for spellId, spell in pairs(selector.Spells) do
+                            if not blacklistedAbilities[spell.Name] then
+                                spells[spell.Name] = {
+                                    Spell = spell,
+                                    ClassLevel = level,
+                                }
+                            end
                         end
                     end
                 end
